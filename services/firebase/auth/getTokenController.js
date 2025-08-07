32b8ce92981455d985febc7f4c1f7a8a22541352
juanmanuel2002 <juanmanuelmirandarayon@gmail.com>
2025-08-06 18:37:14 -0600

    Endpoint para mantener sesion activa

--- services/firebase/auth/getTokenController.js
+++ services/firebase/auth/getTokenController.js
@@ -1,5 +1,7 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../setup.js';
+import config from '../../../config.js';
+import axios from 'axios';
 
 export async function getFirebaseToken(req, res) {
   const { email, password } = req.body;
@@ -15,3 +17,38 @@ export async function getFirebaseToken(req, res) {
     res.status(401).json({ error: error.message });
   }
 }
+
+export async function getRefreshToken(req, res){
+  const { refreshToken } = req.body;
+
+  if (!refreshToken) {
+    return res.status(400).json({ error: 'refreshToken requerido' });
+  }
+  try {
+    const key = config.firebase.apiKey
+   
+    const response = await axios.post(
+      `https://securetoken.googleapis.com/v1/token?key=${key}`,
+      new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: refreshToken,
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      }
+    );
+
+    const { id_token, expires_in } = response.data;
+
+    return res.status(200).json({
+      accessToken: id_token,
+      expirationTime: parseInt(expires_in, 10) * 1000, 
+    });
+
+  } catch (error) {
+    console.error('Error al refrescar token:', error?.response?.data || error.message);
+    return res.status(401).json({ error: 'Token inválido o expirado' });
+  }
+};
--- services/firebase/auth/getTokenRoutes.js
+++ services/firebase/auth/getTokenRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getFirebaseToken } from './getTokenController.js';
+import { getFirebaseToken, getRefreshToken } from './getTokenController.js';
+import { firebaseAuthMiddleware } from '../middleware/firebaseAuthMiddleware.js';
 
 const router = express.Router();
 
 router.post('/token', getFirebaseToken);
+router.post('/refreshToken', firebaseAuthMiddleware, getRefreshToken)
 
 export default router;

ec7b6d5535a0cd13288392077bd28b564dd25143
juanmanuel2002 <juanmanuelmirandarayon@gmail.com>
2025-09-12 18:05:04 -0600

    Agregando logica de admin

--- services/firebase/db/dbRoutes.js
+++ services/firebase/db/dbRoutes.js
@@ -2,23 +2,23 @@ import { sendPurchaseEmail, sendInscripcionClase } from '../../utils/sendEmail.j
 import { getCitas, createCita, updateCita, deleteCita } from './dbController.js';
 import express from 'express';
 import { getCursos, getRecursos, createCurso, createRecurso, updateRecursos, updateCursos, asignarRecursosCursos, getCursosUsuario, getRecursosUsuario, getCitasUsuario} from './dbController.js';
-import { firebaseAuthMiddleware } from '../middleware/firebaseAuthMiddleware.js';
+import { firebaseAuthMiddleware, firebaseAdminMiddleware } from '../middleware/firebaseAuthMiddleware.js';
 import { logAfterResponse } from '../../utils/logService.js'
 
 const router = express.Router();
 
 router.get('/cursos', getCursos);
 router.get('/recursos', getRecursos);
-router.post('/cursos', logAfterResponse('createCurso'), firebaseAuthMiddleware, createCurso );
-router.put('/cursos', logAfterResponse('actualizarCurso'), firebaseAuthMiddleware, updateCursos);
-router.post('/recursos', logAfterResponse('createRecurso'), firebaseAuthMiddleware, createRecurso);
-router.put('/recursos', logAfterResponse('actualizarRecurso'), firebaseAuthMiddleware, updateRecursos);
+router.post('/cursos', logAfterResponse('createCurso'), firebaseAdminMiddleware, createCurso );
+router.put('/cursos', logAfterResponse('actualizarCurso'), firebaseAdminMiddleware, updateCursos);
+router.post('/recursos', logAfterResponse('createRecurso'), firebaseAdminMiddleware, createRecurso);
+router.put('/recursos', logAfterResponse('actualizarRecurso'), firebaseAdminMiddleware, updateRecursos);
 router.post('/asignar-recursos-cursos', logAfterResponse('asignarRecursosCursos'), firebaseAuthMiddleware, asignarRecursosCursos);
 
 router.get('/citas', firebaseAuthMiddleware, getCitas);
 router.post('/citas', logAfterResponse('createCita'), firebaseAuthMiddleware, createCita);
-router.put('/citas', firebaseAuthMiddleware, updateCita);
-router.delete('/citas', firebaseAuthMiddleware, deleteCita);
+router.put('/citas', firebaseAdminMiddleware, updateCita);
+router.delete('/citas', firebaseAdminMiddleware, deleteCita);
 
 router.get('/usuario/:uid/cursos', firebaseAuthMiddleware, getCursosUsuario);
 router.get('/usuario/:uid/recursos', firebaseAuthMiddleware, getRecursosUsuario);
--- services/firebase/middleware/firebaseAuthMiddleware.js
+++ services/firebase/middleware/firebaseAuthMiddleware.js
@@ -24,3 +24,22 @@ export async function firebaseAuthMiddleware(req, res, next) {
     return res.status(401).json({ error: 'No autorizado. Debes iniciar sesión.' });
   }
 }
+
+export async function firebaseAdminMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'No autorizado. Debes iniciar sesión.' });
+  }
+  const idToken = authHeader.split('Bearer ')[1];
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    if (decodedToken.admin === true) {
+      req.user = { uid: decodedToken.uid, admin: true };
+      return next();
+    } else {
+      return res.status(403).json({ error: 'Acceso solo para administradores.' });
+    }
+  } catch (error) {
+    return res.status(401).json({ error: 'Token inválido.' });
+  }
+}
